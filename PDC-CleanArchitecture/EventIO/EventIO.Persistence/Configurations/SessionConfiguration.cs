8c730510a94ddaad8ab8e5612baeed2085a3b421
Patrick-Ullrich <patrick.ullrich93@gmail.com>
2019-10-19 19:09:42 -0600

    Adding Fluent Configuration

---
+++ PDC-CleanArchitecture/EventIO/EventIO.Persistence/Configurations/SessionConfiguration.cs
@@ -0,0 +1,14 @@
+﻿using EventIO.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace EventIO.Persistence.Configurations
+{
+    public class SessionConfiguration : IEntityTypeConfiguration<Session>
+    {
+        public void Configure(EntityTypeBuilder<Session> builder)
+        {
+            builder.Property(e => e.SessionId).HasColumnName("SessionID");
+        }
+    }
+}
